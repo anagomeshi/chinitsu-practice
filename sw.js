0394464b6c71e0e325fb2060aa2d7223533fae03
osechi-1111 <121627718+osechi-1111@users.noreply.github.com>
2024-06-23 16:53:44 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,18 @@
-var CACHE_NAME = 'chinitsu-practice';
+var CACHE_NAME = 'chinitsu-practice-v1';
 var urlsToCache = [
     '/',
+    '/img/check.svg',
+    '/img/s1.png',
+    '/img/s2.png',
+    '/img/s3.png',
+    '/img/s4.png',
+    '/img/s5.png',
+    '/img/s6.png',
+    '/img/s7.png',
+    '/img/s8.png',
+    '/img/s9.png',
+    '/img/setting.svg',
+    '/img/time-attack.svg',
     '/script.js',
     '/style.css'
 ];
@@ -8,11 +20,29 @@ var urlsToCache = [
 // インストール処理
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
+        caches.open(CACHE_NAME).then(function(cache) {
                 return cache.addAll(urlsToCache);
-            })
+        })
+    );
+    self.skipWaiting();
+});
+
+// アクティベート処理
+self.addEventListener('activate', function(event) {
+    var cacheWhitelist = [CACHE_NAME];
+
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(function(){
+            return self.clients.claim();
+        })
     );
 });
 
